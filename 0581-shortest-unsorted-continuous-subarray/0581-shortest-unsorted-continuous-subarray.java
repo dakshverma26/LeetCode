00fc6f7c0ea25d7284f848d00157cf62a7844d28
Daksh Verma <147102293+dakshverma26@users.noreply.github.com>
2026-09-14 22:20:48 +0530

    Time: 1 ms (86.21%), Space: 47.5 MB (35.15%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -8,29 +8,22 @@ class Solution {
 
         int max = nums[0];
 
-        // Left -> Right
-        for (int i = 1; i < n; i++) {
+        int min = nums[n-1];
 
-            if (nums[i] < max) {
-                right = i;
-            } else {
-                max = nums[i];
-            }
-        }
+        for(int i=1 , j=n-2 ; i<n && j>=0 ; i++,j-- )
+        {
+            if( nums[i]<max)
+                right=i;
+            else
+                max=nums[i];
 
-        int min = nums[n - 1];
+            if( nums[j]>min)
+                left=j;
+            else
+                min=nums[j];    
+       }
 
-        // Right -> Left
-        for (int i = n - 2; i >= 0; i--) {
-
-            if (nums[i] > min) {
-                left = i;
-            } else {
-                min = nums[i];
-            }
-        }
-
-        if (right == -1)
+               if (right == -1)
             return 0;
 
         return right - left + 1;
